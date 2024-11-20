e5dee7de664a9dd8e977ed05befdc73bc9255588
Graham Arnett <GrahamKentArnett@outlook.com>
2024-11-20 13:59:06 -0500

    init commit

---
+++ Tic Tac Toe Game/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tic_Tac_Toe_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my tic tac toe game!");
+
+            //we would probably start the loop around here
+
+            //prompt user for where they want to place their mark, user is X, user will always go first
+            //have the computer pick a spot, for the sake of simplicity make this version just pick a spot from a random number
+            
+            //we want the grid printed out and to update with new inputs, not just print again for each new input
+            //3 in a row win, if no 3 in a row is possible the game is a draw
+        }
+    }
+}
