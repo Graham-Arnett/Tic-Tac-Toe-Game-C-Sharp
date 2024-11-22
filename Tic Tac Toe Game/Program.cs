f4eaf7e695fc31fd44ae7d9c5935cfd0b70c5dce
Graham Arnett <GrahamKentArnett@outlook.com>
2024-11-22 12:53:25 -0500

    added the variable we'll use to keep the game going, will work on this more later today when I have time

--- Tic Tac Toe Game/Program.cs
+++ Tic Tac Toe Game/Program.cs
@@ -5,14 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my tic tac toe game!");
-
-            //we would probably start the loop around here
-
+            string choice;
+            //we would probably start the core loop around here
+            do 
+            {
+                
+                Console.Write("Wouold you like to play again?");
+                choice = Console.ReadLine().ToLower();
+            }while(choice =="y");
             //prompt user for where they want to place their mark, user is X, user will always go first
             //have the computer pick a spot, for the sake of simplicity make this version just pick a spot from a random number
-            
+
             //we want the grid printed out and to update with new inputs, not just print again for each new input
             //3 in a row win, if no 3 in a row is possible the game is a draw
+
+            Console.WriteLine("Goodbye, thanks for playing!"); //what displays when the user ends the game
         }
     }
 }
